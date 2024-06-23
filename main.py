a54e97451975ce3dcae9850bbb0e808bfc95b330
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-06-23 13:27:07 -0500

    + class Particle; updtd class Simulation; started mainloop

--- main.py
+++ main.py
@@ -15,13 +15,34 @@ pygame.display.set_caption("Gravity Simulator")
 
 class Particle:
     def __init__(self, x, y, mass, radius):
+        self.x = x
+        self.y = y
+        self.mass = mass
+        self.radius = radius
+        self.vx = 0
+        self.vy = 0
+
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, WHITE, (int(self.x), int(self.y)), self.radius)
+    
+class Simulation:
+    def __init__(self):
+        self.particles = []
+    
+    def add_particle(self, particle):
+        pass
+    
+    def update(self):
+        #need to set gravitational constant; add for loop
         pass
     
     def draw(self, screen):
         pass
     
-class Simulation:
-    def __init__(self):
-        pass
-    
 # Need main game loop
+running = True
+simulation = Simulation()
+
+while running:
+    pass
