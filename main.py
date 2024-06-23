80994cf66dd5dd820a6c56443912fcc679df1d9e
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-06-23 13:09:00 -0500

    setup

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+import math
+
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 800, 600
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# screen setup
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Gravity Simulator")
+
+class Particle:
+    def __init__(self, x, y, mass, radius):
+        pass
+    
+    def draw(self, screen):
+        pass
+    
+class Simulation:
+    def __init__(self):
+        pass
+    
+# Need main game loop
